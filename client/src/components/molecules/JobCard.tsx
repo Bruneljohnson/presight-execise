951f4599021f3f73eddcba18e032c090508d2dfb
Bruneljohnson <photography@bruneljohnson.com>
2026-03-18 17:17:54 +0000

    feat: tweaking app

--- client/src/components/molecules/JobCard.tsx
+++ client/src/components/molecules/JobCard.tsx
@@ -1,15 +1,6 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 
-export interface GeneratedPerson {
-  index: number;
-  first_name: string;
-  last_name: string;
-  age: number;
-  nationality: string;
-  hobbies: string[];
-}
-
 export interface Job {
   requestId: string;
   status: "pending" | "done";
